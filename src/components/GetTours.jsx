db606018451d1978fccad61769f34d54b99a510a
Nettar-Jr <alabiabdulmumuni9@gmail.com>
2021-11-30 20:47:34 +1300

    first commit

---
+++ src/components/GetTours.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+import './GetTours.css';
+
+function GetTours() {
+
+    const [getTours, setgetTours] = useState([])
+
+
+
+    const url = 'https://course-api.com/react-tours-project';
+
+        const fetchData = async () =>{
+        const response = await fetch(url);
+        const data = await response.json();
+
+        setgetTours(data);
+        console.log(data[0]);
+    }
+
+    const creatTours = (getTours)=>{
+        return  <li id={getTours.id}>
+                    <img src={getTours.image} alt="" />
+                    <div className="details footer">
+                        <p className="name-price"><span className="name">{getTours.name}</span><span className="price">{'$' + getTours.price}</span></p>
+                        <p className="info">{getTours.info}</p>
+                        <button className="del-Btn " onClick={delTour} >Not Interested</button>
+                    </div>
+                
+                </li>
+    }
+
+    const delTour = ()=> {
+        
+    }
+
+    window.onload = ()=> {
+        fetchData()
+    }
+
+    let tours = getTours.map(creatTours);
+
+    return (
+    
+        <ul className='tour-contianer'>
+            <h1 className="title">Our Tours
+                <hr />
+            </h1>
+            
+            {tours}         
+        </ul>
+    )
+    
+}
+
+export default GetTours
